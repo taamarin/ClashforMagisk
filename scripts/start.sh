0cf30192b354f80d04a9bc73bee3f127e2e7b5fe
taamarin <juwanda.170310115@mhs.unimal.ac.id>
2022-03-11 00:53:51 +0700

    edit scripts
    - pisah dns/genaral dari config.yaml
    
    - auto copy confog.yaml ke folder run
    
    - untuk mengurangi error karna subconvert

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,25 +1,32 @@
 #!/bin/sh
 
-service_path=`realpath $0`
-module_dir=`dirname ${service_path}`
-scripts_dir="${module_dir}/../../modules/ClashforMagisk"
-Clash_data_dir="/data/adb/clash"
-busybox_path="/data/adb/magisk/busybox"
-Clash_scripts_dir="${Clash_data_dir}/scripts"
-Clash_run_path="${Clash_data_dir}/run"
-Clash_pid_file="${Clash_run_path}/clash.pid"
-
-if [ -f ${Clash_pid_file} ] ; then
-    rm -rf ${Clash_pid_file}
+MODDIR="/data/adb/modules/ClashforMagisk"
+if [ -n "$(magisk -v | grep lite)" ]; then
+  MODDIR=/data/adb/lite_modules/ClashforMagisk
 fi
 
-until [ -f ${module_dir}/clash.config ] ; do
-    sleep 1
-done
+SCRIPTS_DIR="/data/adb/clash/scripts"
+BUSYBOX_PATH="/data/adb/magisk/busybox"
+CLASH_RUN_PATH="/data/adb/clash/run"
+CLASH_PID_FILE="${CLASH_RUN_PATH}/clash.pid"
 
-nohup ${busybox_path} crond -c ${Clash_run_path} > /dev/null 2>&1 &
-
-if [ ! -f ${scripts_dir}/disable ] ; then
-  ${module_dir}/clash.service -s && ${module_dir}/clash.tproxy -s
+if [ -f ${CLASH_PID_FILE} ] ; then
+    rm -rf ${CLASH_PID_FILE}
 fi
-inotifyd ${module_dir}/clash.inotify ${scripts_dir} >> /dev/null &
+
+# nohup ${BUSYBOX_PATH} crond -c ${CLASH_RUN_PATH} > /dev/null 2>&1 &
+
+start_tproxy () {
+  ${SCRIPTS_DIR}/clash.service -s
+  if [ -f /data/adb/clash/run/clash.pid ] ; then
+    ${SCRIPTS_DIR}/clash.tproxy -s
+  fi
+}
+
+if [ ! -f /data/adb/clash/manual ] ; then
+  echo -n "" > /data/adb/clash/run/service.log
+  if [ ! -f ${MODDIR}/disable ] ; then
+      start_tproxy
+  fi
+  inotifyd ${SCRIPTS_DIR}/clash.inotify ${MODDIR}::nd &>> /dev/null &
+fi
