81bba17bee9bee6f2ae0a006ad8e490f715abf4a
taamarin <juwanda.170310115@mhs.unimal.ac.id>
2022-05-07 07:17:04 +0700

    update scripts

--- customize.sh
+++ customize.sh
@@ -43,7 +43,7 @@ mkdir -p ${clash_data_dir}/yacd-gh-pages
 mkdir -p ${MODPATH}/system/bin
 mkdir -p ${clash_data_dir}/run
 mkdir -p ${clash_data_dir}/scripts
-mkdir -p ${clash_data_dir}/config
+mkdir -p ${clash_data_dir}/switch
 
 case "${ARCH}" in
     arm)
