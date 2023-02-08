a125dc1ea3d4ef77a9dc396655e1fa5adabcb328
llyc <juwandafly@gmail.com>
2023-02-08 14:52:35 +0700

    fix

--- scripts/usage.sh
+++ scripts/usage.sh
@@ -1,14 +1,16 @@
 #!/system/bin/sh
 
+export PATH=$(magisk --path)/.magisk/busybox:$PATH:/system/bin
+
 getmemory(){
   local clash_pid=$(cat /data/clash/run/clash.pid)
-  clash_alive=$(grep VmRSS /proc/${clash_pid}/status | /data/adb/magisk/busybox awk -F':' '{print $2}' | /data/adb/magisk/busybox awk '{print $1}')
+  clash_alive=$(grep VmRSS /proc/${clash_pid}/status | awk -F':' '{print $2}' | awk '{print $1}')
   if [ ${clash_alive} -ge 1024 ] ; then
     clash_res="$(expr ${clash_alive} / 1024)Mb"
   else
     clash_res="${clash_alive}Kb"
   fi
-  clash_cpu=$(ps -p ${clash_pid} -o pcpu | grep -v %CPU | awk '{print $1}' )%
+  clash_cpu=$(/system/bin/ps -p ${clash_pid} -o pcpu | grep -v %CPU | awk '{print $1}' )%
   log_usage="CPU: ${clash_cpu} | RES: ${clash_res}" 
   sed -i "s/CPU:.*/${log_usage}/" /data/clash/run/run.logs
 }
